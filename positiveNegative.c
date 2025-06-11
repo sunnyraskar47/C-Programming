cd27387402b8fb49907c8c67b8addb187f88cb7c
Sunny Raskar <sunnyraskar47@gmail.com>
2025-06-11 13:31:01 +0530

    positiveNegative

---
+++ positiveNegative.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main ()
+{
+    int n;
+    
+    printf("Enter a Number");
+    scanf("%d",&n);
+
+    if(n>0)
+    printf("positive No");
+
+    else
+    printf("negative No");
+
+    return 0;
+}
---
+++ ternary.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main ()
+{
+    int n;
+    printf("Enter a number : ");
+    scanf("%d",&n);
+
+    (n>0)?printf("positive No"):printf("negative No");
+    return 0;
+}
